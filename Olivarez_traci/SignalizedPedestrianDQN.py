5e8424e67aab51eb2238c1dbf7b320b6423779e3
Jack Elinzano <113414920+jcklzn@users.noreply.github.com>
2025-11-12 06:11:22 +0800

    resolved conflicts

--- Olivarez_traci/SignalizedPedestrianDQN.py
+++ Olivarez_traci/SignalizedPedestrianDQN.py
@@ -7,18 +7,16 @@ from keras.utils import to_categorical
 
 from models.DQN import DQNAgent as dqn
 
-### --- CHANGED --- ###
-# State sizes are recalculated based on exclusive states.
-# Main Agent: 12 queues (11 car + 1 ped) + 9 phases (5+2+2) = 21
-# Ped Agents: 1 queue (1 ped) + 9 phases (5+2+2) = 10
-mainIntersectionAgent = dqn(state_size=21, action_size=11, memory_size=200, gamma=0.95, epsilon=1, epsilon_decay_rate=0.995, epsilon_min=0.01, learning_rate=0.00005, target_update_freq=500, name='ReLU_DQNAgent')
-swPedXingAgent = dqn(state_size=10, action_size=11, memory_size=200, gamma=0.95, epsilon=1, epsilon_decay_rate=0.995, epsilon_min=0.01, learning_rate=0.00005, target_update_freq=500, name='SW_PedXing_Agent')
-sePedXingAgent = dqn(state_size=10, action_size=11, memory_size=200, gamma=0.95, epsilon=1, epsilon_decay_rate=0.995, epsilon_min=0.01, learning_rate=0.00005, target_update_freq=500, name='SE_PedXing_Agent')
+mainIntersectionAgent = dqn(state_size=17, action_size=11, memory_size=2000, gamma=0.95, epsilon=0.01, epsilon_decay_rate=0.995, epsilon_min=0.001, learning_rate=0.00005, target_update_freq=500, name='ReLU_DQNAgent')
+swPedXingAgent = dqn(state_size=14, action_size=11, memory_size=2000, gamma=0.95, epsilon=0.01, epsilon_decay_rate=0.995, epsilon_min=0.001, learning_rate=0.00005, target_update_freq=500, name='SW_PedXing_Agent')
+sePedXingAgent = dqn(state_size=14, action_size=11, memory_size=2000, gamma=0.95, epsilon=0.01, epsilon_decay_rate=0.995, epsilon_min=0.001, learning_rate=0.00005, target_update_freq=500, name='SE_PedXing_Agent')
+mainIntersectionAgent = dqn(state_size=17, action_size=11, memory_size=2000, gamma=0.95, epsilon=0, epsilon_decay_rate=0.995, epsilon_min=0.001, learning_rate=0.00005, target_update_freq=500, name='ReLU_DQNAgent')
+swPedXingAgent = dqn(state_size=14, action_size=11, memory_size=2000, gamma=0.95, epsilon=0, epsilon_decay_rate=0.995, epsilon_min=0.001, learning_rate=0.00005, target_update_freq=500, name='SW_PedXing_Agent')
+sePedXingAgent = dqn(state_size=14, action_size=11, memory_size=2000, gamma=0.95, epsilon=0, epsilon_decay_rate=0.995, epsilon_min=0.001, learning_rate=0.00005, target_update_freq=500, name='SE_PedXing_Agent')
 
-# Start fresh training
-# mainIntersectionAgent.load()
-# swPedXingAgent.load()
-# sePedXingAgent.load()
+mainIntersectionAgent.load()
+swPedXingAgent.load()
+sePedXingAgent.load()
 
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -29,6 +27,7 @@ else:
 Sumo_config = [
     'sumo',
     '-c', 'Olivarez_traci\map.sumocfg',
+    '-c', 'Olivarez_traci\signalizedPed.sumocfg',
     '--step-length', '0.05',
     '--delay', '0',
     '--lateral-resolution', '0.1',
@@ -38,6 +37,7 @@ Sumo_config = [
 
 # Simulation Variables
 trainMode = 1
+trainMode = 0
 stepLength = 0.05
 mainCurrentPhase = 0
 mainCurrentPhaseDuration = 30
@@ -85,15 +85,15 @@ def _junctionSubscription(junction_id):
         10.0,
         [traci.constants.VAR_WAITING_TIME]
     )
-    
+
 def _subscribe_all_detectors():
     detector_ids = [
         "e2_4", "e2_5", "e2_6", "e2_7", "e2_8", "e2_9", "e2_10",
         "e2_0", "e2_1", "e2_2", "e2_3"
     ]
-    
+
     vehicle_vars = [traci.constants.VAR_TYPE, traci.constants.VAR_WAITING_TIME]
-    
+
     for det_id in detector_ids:
         traci.lanearea.subscribeContext(
             det_id,
@@ -113,7 +113,7 @@ def _weighted_waits(detector_id):
     for data in vehicle_data.values():
         type = data.get(traci.constants.VAR_TYPE, "car")
         waitTime = data.get(traci.constants.VAR_WAITING_TIME, 0)
-        
+
         if type == "car":
             sumWait += waitTime
         elif type == "jeep":
@@ -128,14 +128,7 @@ def _weighted_waits(detector_id):
             sumWait += waitTime * 0.5
     return sumWait
 
-### --- CHANGED --- ###
-# Main agent now "owns" ALL vehicle detectors
 def _mainIntersection_queue():
-    # All 11 car detectors
-    e2_0 = _weighted_waits("e2_0")
-    e2_1 = _weighted_waits("e2_1")
-    e2_2 = _weighted_waits("e2_2")
-    e2_3 = _weighted_waits("e2_3")
     e2_4 = _weighted_waits("e2_4") 
     e2_5 = _weighted_waits("e2_5")
     e2_6 = _weighted_waits("e2_6")
@@ -143,68 +136,60 @@ def _mainIntersection_queue():
     e2_8 = _weighted_waits("e2_8")
     e2_9 = _weighted_waits("e2_9")
     e2_10 = _weighted_waits("e2_10")
-    
+
     pedestrian = 0
     junction_subscription = traci.junction.getContextSubscriptionResults("cluster_295373794_3477931123_7465167861")
-    
+
     if junction_subscription:
         for pid, data in junction_subscription.items():
             pedestrian += data.get(traci.constants.VAR_WAITING_TIME, 0)
-            
-    # Return all 12 queue values
-    return [e2_0, e2_1, e2_2, e2_3, e2_4, e2_5, e2_6, e2_7, e2_8, e2_9, e2_10, pedestrian]
 
-### --- CHANGED --- ###
-# SW Ped agent now "owns" ONLY its pedestrian detector
+    return [e2_4, e2_5, e2_6, e2_7, e2_8, e2_9, e2_10, pedestrian]
+
 def _swPedXing_queue():
-    # e2_0 = _weighted_waits("e2_0") # <-- REMOVED
-    # e2_1 = _weighted_waits("e2_1") # <-- REMOVED
-    # e2_4 = _weighted_waits("e2_4") # <-- REMOVED
-    # e2_5 = _weighted_waits("e2_5") # <-- REMOVED
+    e2_0 = _weighted_waits("e2_0")
+    e2_1 = _weighted_waits("e2_1")
+    e2_4 = _weighted_waits("e2_4")
+    e2_5 = _weighted_waits("e2_5")
     pedestrian = 0
     junction_subscription = traci.junction.getContextSubscriptionResults("6401523012")
-    
+
     if junction_subscription:
         for pid, data in junction_subscription.items():
             pedestrian += data.get(traci.constants.VAR_WAITING_TIME, 0)
-            
-    return [pedestrian] # State is ONLY pedestrian queue
 
-### --- CHANGED --- ###
-# SE Ped agent now "owns" ONLY its pedestrian detector
+    return [e2_0, e2_1, e2_4, e2_5, pedestrian]
+
 def _sePedXing_queue():
-    # e2_2 = _weighted_waits("e2_2") # <-- REMOVED
-    # e2_3 = _weighted_waits("e2_3") # <-- REMOVED
-    # e2_6 = _weighted_waits("e2_6") # <-- REMOVED
-    # e2_7 = _weighted_waits("e2_7") # <-- REMOVED
+    e2_2 = _weighted_waits("e2_2")
+    e2_3 = _weighted_waits("e2_3")
+    e2_6 = _weighted_waits("e2_6")
+    e2_7 = _weighted_waits("e2_7")
     pedestrian = 0
     junction_subscription = traci.junction.getContextSubscriptionResults("3285696417")
-    
+
     if junction_subscription:
         for pid, data in junction_subscription.items():
             pedestrian += data.get(traci.constants.VAR_WAITING_TIME, 0)
 
-    return [pedestrian] # State is ONLY pedestrian queue
+    return [e2_2, e2_3, e2_6, e2_7, pedestrian]
 
-# This reward function is perfect and works for all agents
-def calculate_reward(current_state_queues):
-    if current_state_queues is None:
+def calculate_reward(current_state):
+    if current_state is None:
         print("ERROR: STATE UNDETECETED")
         return 0
-    
-    # current_state_queues will be [pedestrian] for ped agents
-    # and [e2_0, ..., pedestrian] for main agent. sum() works perfectly.
-    current_total = sum(current_state_queues)
+
+    current_total = sum(current_state)
     return -current_total
 
 def _mainIntersection_phase(action_index):
     global mainCurrentPhase, mainCurrentPhaseDuration
-    
+
     mainCurrentPhase += 1
     mainCurrentPhase = mainCurrentPhase % 10
-    
+
     traci.trafficlight.setPhase("cluster_295373794_3477931123_7465167861", mainCurrentPhase)
-    
+
     if mainCurrentPhase % 2 == 1:
         phase_duration = 5
     else:
@@ -213,22 +198,26 @@ def _mainIntersection_phase(action_index):
             base_duration = 15
         else:
             base_duration = 30
-        
+
         phase_duration = max(5, min(60, base_duration + duration_adjustment))
-    
+
     mainCurrentPhaseDuration = phase_duration
     traci.trafficlight.setPhaseDuration("cluster_295373794_3477931123_7465167861", mainCurrentPhaseDuration)
-    
+
 def _swPedXing_phase(action_index):
     global swCurrentPhase, swCurrentPhaseDuration
-    
+
     swCurrentPhase += 1
     swCurrentPhase = swCurrentPhase % 4
-    
+
     traci.trafficlight.setPhase("6401523012", swCurrentPhase)
 
     if swCurrentPhase % 2 == 1:
         phase_duration = 5
+    elif swCurrentPhase == 0:
+        duration_adjustment = actionSpace[action_index]
+        base_duration = 100
+        phase_duration = max(5, min(130, base_duration + duration_adjustment))
     else:
         duration_adjustment = actionSpace[action_index]
         base_duration = 30
@@ -236,23 +225,27 @@ def _swPedXing_phase(action_index):
 
     swCurrentPhaseDuration = phase_duration
     traci.trafficlight.setPhaseDuration("6401523012", swCurrentPhaseDuration)
-    
+
 def _sePedXing_phase(action_index):
     global seCurrentPhase, seCurrentPhaseDuration
-    
+
     seCurrentPhase += 1
     seCurrentPhase = seCurrentPhase % 4
-    
+
     traci.trafficlight.setPhase("3285696417", seCurrentPhase)
-    
-    
+
+
     if seCurrentPhase % 2 == 1:
         phase_duration = 5
+    elif seCurrentPhase == 0:
+        duration_adjustment = actionSpace[action_index]
+        base_duration = 100
+        phase_duration = max(5, min(130, base_duration + duration_adjustment))
     else:
         duration_adjustment = actionSpace[action_index]
         base_duration = 30
         phase_duration = max(5, min(60, base_duration + duration_adjustment))
-    
+
     seCurrentPhaseDuration = phase_duration
     traci.trafficlight.setPhaseDuration("3285696417", seCurrentPhaseDuration)
 
@@ -267,6 +260,14 @@ def save_history(filename, headers, reward_hist, loss_hist, epsilon_hist, train_
             for i in range(len(reward_hist)):
             # Save the simulation step number (i * frequency)
                 writer.writerow([i * train_frequency, reward_hist[i], loss_hist[i], epsilon_hist[i]])
+
+
+
+
+
+
+
+
 
 traci.start(Sumo_config)
 _subscribe_all_detectors()
@@ -286,95 +287,83 @@ while traci.simulation.getMinExpectedNumber() > 0:
     mainCurrentPhaseDuration -= stepLength
     swCurrentPhaseDuration -= stepLength
     seCurrentPhaseDuration -= stepLength
-    
+
     #Observation and Reward
     if mainCurrentPhaseDuration <= 0:
         if mainCurrentPhase % 2 == 0:
-            ### --- CHANGED --- ###
-            # main_queue now has 12 elements
             main_queue = np.array(_mainIntersection_queue())
             normalized_main_queue = main_queue/1000
             main_phase = to_categorical(mainCurrentPhase//2, num_classes=5).flatten()
-            
-            # This is the state - it has 12 + 5 + 2 + 2 = 21 elements
             mainCurrentState = np.concatenate([normalized_main_queue, main_phase, swPed_phase, sePed_phase]).astype(np.float32)
-            
+
             if trainMode == 1:
-                # Reward is based on all 12 queues
-                mainReward = calculate_reward(main_queue) # Pass raw queue for reward
+                mainReward = calculate_reward(normalized_main_queue)
                 total_main_reward += mainReward
-                
+
                 if mainPrevState is not None and mainPrevAction is not None:
                     done = False
                     mainIntersectionAgent.remember(mainPrevState, mainPrevAction, mainReward, mainCurrentState, done)
-    
+
     if swCurrentPhaseDuration <= 0:
         if swCurrentPhase % 2 == 0:
-            ### --- CHANGED --- ###
-            # swPed_queue now has 1 element
             swPed_queue = np.array(_swPedXing_queue())
             normalized_swPed_queue = swPed_queue/1000
             swPed_phase = to_categorical(swCurrentPhase//2, num_classes=2).flatten()
-            
-            # This is the state - it has 1 + 5 + 2 + 2 = 10 elements
             swCurrentState = np.concatenate([normalized_swPed_queue, main_phase, swPed_phase, sePed_phase]).astype(np.float32)
-            
+
             if trainMode == 1:
-                # Reward is based only on the 1 pedestrian queue
-                swReward = calculate_reward(swPed_queue) # Pass raw queue for reward
+                swReward = calculate_reward(normalized_swPed_queue)
                 total_sw_reward += swReward
-                
+
                 if swPrevState is not None and swPrevAction is not None:
                     done = False
                     swPedXingAgent.remember(swPrevState, swPrevAction, swReward, swCurrentState, done)
-    
+
     if seCurrentPhaseDuration <= 0:
         if seCurrentPhase % 2 == 0:
-            ### --- CHANGED --- ###
-            # sePed_queue now has 1 element
             sePed_queue = np.array(_sePedXing_queue())
             normalized_sePed_queue = sePed_queue/1000
             sePed_phase = to_categorical(seCurrentPhase//2, num_classes=2).flatten()
-            
-            # This is the state - it has 1 + 5 + 2 + 2 = 10 elements
             seCurrentState = np.concatenate([normalized_sePed_queue, main_phase, swPed_phase, sePed_phase]).astype(np.float32)
-            
+
             if trainMode == 1:
-                # Reward is based only on the 1 pedestrian queue
-                seReward = calculate_reward(sePed_queue) # Pass raw queue for reward
+                seReward = calculate_reward(normalized_sePed_queue)
                 total_se_reward += seReward
-                
+
                 if sePrevState is not None and sePrevAction is not None:
                     done = False
                     sePedXingAgent.remember(sePrevState, sePrevAction, seReward, seCurrentState, done)
-    
+
+
+
+
     #Action
     if mainCurrentPhaseDuration <= 0:
         if mainCurrentPhase % 2 == 0:
             mainActionIndex = mainIntersectionAgent.act(mainCurrentState)
             mainPrevState = mainCurrentState
             mainPrevAction = mainActionIndex
-            
+
             if trainMode == 1:
-                # Pass normalized queue for printing
-                print(f"Main Intersection - Queue: {sum(normalized_main_queue):.2f}, Reward: {mainReward:.2f}, Action: {actionSpace[mainActionIndex]}")
+                print(f"Main Intersection - Queue: {sum(normalized_main_queue)}, Reward: {mainReward}, Action: {actionSpace[mainActionIndex]}")
         else:
             mainActionIndex = mainPrevAction
 
         _mainIntersection_phase(mainActionIndex)
-        
+
+
+
     if swCurrentPhaseDuration <= 0:
         if swCurrentPhase % 2 == 0:
             swActionIndex = swPedXingAgent.act(swCurrentState)
             swPrevState = swCurrentState
             swPrevAction = swActionIndex
-            
+
             if trainMode == 1:
-                # Pass normalized queue for printing
-                print(f"SW Ped Crossing - Queue: {sum(normalized_swPed_queue):.2f}, Reward: {swReward:.2f}, Action: {actionSpace[swActionIndex]}")
+                print(f"SW Ped Crossing - Queue: {sum(normalized_swPed_queue)}, Reward: {swReward}, Action: {actionSpace[swActionIndex]}")
         else:
             swActionIndex = swPrevAction
-        
+
         _swPedXing_phase(swActionIndex)
 
     if seCurrentPhaseDuration <= 0:
@@ -382,14 +371,14 @@ while traci.simulation.getMinExpectedNumber() > 0:
             seActionIndex = sePedXingAgent.act(seCurrentState)
             sePrevState = seCurrentState
             sePrevAction = seActionIndex
-            
+
             if trainMode == 1:
-                # Pass normalized queue for printing
-                print(f"SE Ped Crossing - Queue: {sum(normalized_sePed_queue):.2f}, Reward: {seReward:.2f}, Action: {actionSpace[seActionIndex]}")
+                print(f"SE Ped Crossing - Queue: {sum(normalized_sePed_queue)}, Reward: {seReward}, Action: {actionSpace[seActionIndex]}")
         else:
             seActionIndex = sePrevAction
-        
+
         _sePedXing_phase(seActionIndex)
+
 
     # Periodic training (replay)
     if trainMode == 1 and step_counter % TRAIN_FREQUENCY == 0:
@@ -400,8 +389,8 @@ while traci.simulation.getMinExpectedNumber() > 0:
             main_epsilon_history.append(mainIntersectionAgent.epsilon)
             total_main_reward = 0
             mainIntersectionAgent.epsilon = max(mainIntersectionAgent.epsilon_min, 
-                                                mainIntersectionAgent.epsilon * mainIntersectionAgent.epsilon_decay_rate)
-        
+                                               mainIntersectionAgent.epsilon * mainIntersectionAgent.epsilon_decay_rate)
+
         if len(swPedXingAgent.memory) >= BATCH_SIZE:
             loss = swPedXingAgent.replay(BATCH_SIZE)
             sw_loss_history.append(loss)
@@ -410,7 +399,7 @@ while traci.simulation.getMinExpectedNumber() > 0:
             total_sw_reward = 0
             swPedXingAgent.epsilon = max(swPedXingAgent.epsilon_min,
                                         swPedXingAgent.epsilon * swPedXingAgent.epsilon_decay_rate)
-        
+
         if len(sePedXingAgent.memory) >= BATCH_SIZE:
             loss = sePedXingAgent.replay(BATCH_SIZE)
             se_loss_history.append(loss)
@@ -419,8 +408,12 @@ while traci.simulation.getMinExpectedNumber() > 0:
             total_se_reward = 0
             sePedXingAgent.epsilon = max(sePedXingAgent.epsilon_min,
                                         sePedXingAgent.epsilon * sePedXingAgent.epsilon_decay_rate)
-    
+
     traci.simulationStep()
+
+
+
+
 
 if trainMode == 1:
     # Save trained models
@@ -432,14 +425,12 @@ if trainMode == 1:
 
     print("Saving training history...")
     save_history('./Olivarez_traci/output_DQN/main_agent_history.csv', ['Step', 'Reward', 'Loss', 'Epsilon'], 
-                 main_reward_history, main_loss_history, main_epsilon_history, TRAIN_FREQUENCY)
-                 
+                main_reward_history, main_loss_history, main_epsilon_history, TRAIN_FREQUENCY)
+
     save_history('./Olivarez_traci/output_DQN/sw_agent_history.csv', ['Step', 'Reward', 'Loss', 'Epsilon'], 
-                 sw_reward_history, sw_loss_history, sw_epsilon_history, TRAIN_FREQUENCY)
-                 
+                sw_reward_history, sw_loss_history, sw_epsilon_history, TRAIN_FREQUENCY)
+
     save_history('./Olivarez_traci/output_DQN/se_agent_history.csv', ['Step', 'Reward', 'Loss', 'Epsilon'], 
-                 se_reward_history, se_loss_history, se_epsilon_history, TRAIN_FREQUENCY)
+                se_reward_history, se_loss_history, se_epsilon_history, TRAIN_FREQUENCY)
 
     print("History saved successfully!")
-
-traci.close()
