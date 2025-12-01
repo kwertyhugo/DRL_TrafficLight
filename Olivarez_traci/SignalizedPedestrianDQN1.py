e99d9ea1eb2fd76fa180e1d86752d054046d7dd9
kwertyhugo <kurt_justine_j_hugo@dlsu.edu.ph>
2025-12-01 20:46:15 +0800

    more dqn training

--- Olivarez_traci/SignalizedPedestrianDQN1.py
+++ Olivarez_traci/SignalizedPedestrianDQN1.py
@@ -7,7 +7,7 @@ from keras.utils import to_categorical
 
 from models.DQN import DQNAgent as dqn
 
-trafficLightAgent = dqn(state_size=19, action_size=11, memory_size=2000, gamma=0.95, epsilon=0, epsilon_decay_rate=0.995, epsilon_min=0, learning_rate=0.00005, target_update_freq=500, name='ReLU_DQNAgent1')
+trafficLightAgent = dqn(state_size=19, action_size=11, memory_size=2000, gamma=0.95, epsilon=0.1, epsilon_decay_rate=0.995, epsilon_min=0.1, learning_rate=0.00005, target_update_freq=500, name='ReLU_DQNAgent1')
 
 trafficLightAgent.load()
 
